d70d52b73fac7151bee0db291f50e3d3a393c106
Eduardo Quirino <eduardo.araujo.quirino@hotmail.com>
2023-11-30 17:39:30 -0300

    initial config

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./routes/App";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import Dashboard from "./routes/Dashboard";
-import ErrorPage from "./routes/ErrorPage";
-import Login from "./routes/Login";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
-      },
-    ],
-  },
-]);
+import App from "./pages/App";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </React.StrictMode>
 );
