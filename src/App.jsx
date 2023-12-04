509f2ce8436057e6cca7fad281ead8011a48ca29
Eduardo Quirino <eduardo.araujo.quirino@hotmail.com>
2023-12-04 11:14:33 -0300

    new organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import RoutesApp from "./routes/index";
+import { AuthProvider } from "./contexts/auth";
 function App() {
   return (
-    <>
+    <AuthProvider>
       <RoutesApp />
-    </>
+    </AuthProvider>
 
   );
 }
