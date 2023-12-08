b374dce36bf25282ce7af0c005673bf70c470440
edquirino08 <101748384+edquirino08@users.noreply.github.com>
2023-12-07 23:06:18 -0300

    login route

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,23 +1,52 @@
 import React from 'react';
-import './login.css';
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 import LogoImg from '../../img/teste.png';
+import './login.css';
 
 const Login = () => {
+  const { signin } = useAuth();
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    console.log(password);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = signin(email, password);
+    console.log('res', res);
+
+  }
+
+
   return (
     <div className='paper-container'>
       <img src={LogoImg} alt='Logo' className='logo' />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='email' className='inputLabel' >Email</label>
+        <input className='mailInput' type='email'
+          value={email}
+          onChange={handleEmailChange} />
 
-      <label htmlFor='email' className='inputLabel'>Email</label>
-      <input className='mailInput' />
+        <label htmlFor='password' className='inputLabel'>Senha</label>
+        <input className='passwordInput' type='password'
+          value={password}
+          onChange={handlePasswordChange} />
 
-      <label htmlFor='password' className='inputLabel'>Senha</label>
-      <input className='passwordInput' />
-
-      <button>Entrar</button>
+        <button className='loginButton'>Entrar</button>
+      </form>
 
       <a href='/cadastro'>Criar uma conta</a>
       <a href='/esqueceu-a-senha'>Esqueceu a senha?</a>
-    </div>
+    </div >
   );
 }
 
