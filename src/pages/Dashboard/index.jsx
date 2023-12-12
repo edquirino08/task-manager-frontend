021b1c0e3cf6de05397adc9b7e8976eee6a8f6b8
Eduardo Quirino <eduardo.araujo.quirino@hotmail.com>
2023-12-12 11:53:29 -0300

    new styles imp

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import LogoImg from '../../img/teste.png';
+import './dashboard.css';
+import useAuth from '../../hooks/useAuth';
 
 const Dashboard = () => {
+  const { signout } = useAuth();
+
+  const handleLogout = () => {
+    signout();
+  };
+
   return (
-    <div>Dashboard</div>
-  )
+    <div className='dashboard-container'>
+      <div className='top-bar'>
+        <img src={LogoImg} alt='Logo' className='logo' />
+        <div className='logout-container'>
+          <button className='logout' onClick={handleLogout}>
+            <FontAwesomeIcon icon={faSignOutAlt} />
+            Logout
+          </button>
+        </div>
+      </div>
+      <div className='paper-container'>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
