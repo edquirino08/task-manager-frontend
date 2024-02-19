013ff13ddcfb324a5f8801bf06f0cf3ba8c9b7b9
edquirino08 <eduardo.araujo.quirino@hotmail.com>
2024-02-19 16:33:19 -0300

    dates on task imp

--- src/components/TasksTable/index.jsx
+++ src/components/TasksTable/index.jsx
@@ -3,6 +3,7 @@ import api from '../../services/Api';
 import Search from '../Search';
 import './taskstable.css';
 import { BsCircleFill } from 'react-icons/bs';
+import { format } from 'date-fns';
 
 const TasksTable = () => {
     const [tasks, setTasks] = React.useState([]);
@@ -35,39 +36,74 @@ const TasksTable = () => {
             <Search search={search} setSearch={setSearch} />
             <div className="tasks-container">
                 <div className="paper-pending">
-                    <h3>Pendente</h3>
+                    <h3>
+                        Pendente
+                        <BsCircleFill style={{ color: '#f5e664', fontSize: '10px', marginLeft: '5px' }} />
+                    </h3>
                     {tasks.filter(task => task.task.toLowerCase().includes(search.toLowerCase())).map(task => task.status === 0 && (
                         <div className='pending-task' key={task.id}>
                             <div className='task-content'>
-                                <p className='task'>{task.task}</p>
-                                <p className='category'>({task.category})</p>
-                            </div>
-                            <div className='task-status'>
-                                <BsCircleFill  style={{ color: '#f5e664', fontSize: '10px' }} />
+                                <div>
+                                    <p className='task'>{task.task}</p>
+                                    <p className='category'>({task.category})</p>
+                                </div>
+                                <div className="dates-container">
+                                    <div>
+                                        <p className='date-create'>Criação: {format(new Date(task.date_create), 'dd/MM/yyyy HH:mm')}</p>
+                                    </div>
+                                    <div>
+                                        <p className='date-mod'>Última mod: {format(new Date(task.status_date), 'dd/MM/yyyy HH:mm')}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     ))}
                 </div>
                 <div className="paper-in-progress">
-                    <h3>Em Progresso</h3>
+                    <h3>
+                        Em Progresso
+                        <BsCircleFill style={{ color: '#0e96f1', fontSize: '10px', marginLeft: '5px' }} />
+                    </h3>
                     {tasks.filter(task => task.task.toLowerCase().includes(search.toLowerCase())).map(task => task.status === 1 && (
                         <div className='in-progress-task' key={task.id}>
                             <div className='task-content'>
-                                <p className='task'>{task.task}</p>
-                                <p className='category'>({task.category})</p>
-                            </div>
-                            <div className='task-status'>
-                                <BsCircleFill  style={{ color: '#0e96f1', fontSize: '10px' }} />
+                                <div>
+                                    <p className='task'>{task.task}</p>
+                                    <p className='category'>({task.category})</p>
+                                </div>
+                                <div className="dates-container">
+                                    <div>
+                                        <p className='date-create'>Criação: {format(new Date(task.date_create), 'dd/MM/yyyy HH:mm')}</p>
+                                    </div>
+                                    <div>
+                                        <p className='date-mod'>Última mod: {format(new Date(task.status_date), 'dd/MM/yyyy HH:mm')}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     ))}
                 </div>
                 <div className="paper-done-task">
-                    <h3>Concluída</h3>
+                    <h3>
+                        Concluída
+                        <BsCircleFill style={{ color: '#70e070', fontSize: '10px', marginLeft: '5px' }} />
+                    </h3>
                     {tasks.filter(task => task.task.toLowerCase().includes(search.toLowerCase())).map(task => task.status === 2 && (
                         <div className='done-task' key={task.id}>
-                            <p className='task'>{task.task}</p>
-                            <p className='category'>({task.category})</p>
+                            <div className='task-content'>
+                                <div>
+                                    <p className='task'>{task.task}</p>
+                                    <p className='category'>({task.category})</p>
+                                </div>
+                                <div className="dates-container">
+                                    <div>
+                                        <p className='date-create'>Criação: {format(new Date(task.date_create), 'dd/MM/yyyy HH:mm')}</p>
+                                    </div>
+                                    <div>
+                                        <p className='date-mod'>Última mod: {format(new Date(task.status_date), 'dd/MM/yyyy HH:mm')}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </div>
