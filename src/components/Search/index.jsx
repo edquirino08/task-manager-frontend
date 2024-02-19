434d39b69ef452650ac57d89d5b2cf7f57054d23
edquirino08 <eduardo.araujo.quirino@hotmail.com>
2024-02-19 10:21:32 -0300

    search imp

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Search = ({ search, setSearch }) => {
+    return (
+        <div className=' Search'>
+            <h2>Pesquisar:</h2>
+            <input type='text' value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder='Digite para pesquisar...'
+            />
+        </div>
+    )
+}
+
+export default Search
--- src/components/TasksTable/index.jsx
+++ src/components/TasksTable/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import api from '../../services/Api';
+import Search from '../Search';
+import './taskstable.css';
 
 const TasksTable = () => {
     const [tasks, setTasks] = React.useState([]);
     const userData = JSON.parse(localStorage.getItem('userData'));
+    const [search, setSearch] = React.useState('');
 
     React.useEffect(() => {
         if (userData) {
@@ -27,29 +30,30 @@ const TasksTable = () => {
 
     return (
         <div>
+            <Search search={search} setSearch={setSearch} />
             <ul>
-                <div className="paper">
+                <div className="paper-pending">
                     <h3>Pendente</h3>
-                    {tasks.map(task => task.status === 0 && (
-                        <div className='task-container pending-task' key={task.id}>
+                    {tasks.filter(task => task.task.toLowerCase().includes(search.toLowerCase())).map(task => task.status === 0 && (
+                        <div className='pending-task' key={task.id}>
                             <p className='task'>{task.task}</p>
                             <p className='category'>({task.category})</p>
                         </div>
                     ))}
                 </div>
-                <div className="paper">
+                <div className="paper-in-progress">
                     <h3>Em Progresso</h3>
-                    {tasks.map(task => task.status === 1 && (
-                        <div className='task-container inProgress-task' key={task.id}>
+                    {tasks.filter(task => task.task.toLowerCase().includes(search.toLowerCase())).map(task => task.status === 1 && (
+                        <div className='in-progress-task' key={task.id}>
                             <p className='task'>{task.task}</p>
                             <p className='category'>({task.category})</p>
                         </div>
                     ))}
                 </div>
-                <div className="paper">
+                <div className="paper-done-task">
                     <h3>Concluída</h3>
-                    {tasks.map(task => task.status === 2 && (
-                        <div className='task-container done-task' key={task.id}>
+                    {tasks.filter(task => task.task.toLowerCase().includes(search.toLowerCase())).map(task => task.status === 2 && (
+                        <div className='done-task' key={task.id}>
                             <p className='task'>{task.task}</p>
                             <p className='category'>({task.category})</p>
                         </div>
