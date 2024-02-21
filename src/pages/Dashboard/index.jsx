23bd142015ec4b096bf98f7db95de5e4e59de11d
edquirino08 <eduardo.araujo.quirino@hotmail.com>
2024-02-21 09:58:53 -0300

    fix rolling page

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -5,31 +5,27 @@ import LogoImg from '../../img/teste.png';
 import { FiSettings, FiLogOut } from 'react-icons/fi';
 import TasksTable from '../../components/TasksTable';
 
-
 const Dashboard = () => {
   const { signout } = useAuth();
-
-  // React.useEffect(() => {
-  //   const userData = location.state?.userData;
-  //   console.log(userData);
-  // }, [location]);
-
 
   const handleLogout = () => {
     signout();
   };
 
-
   return (
-    <div className='dashboard-container'>
-      <div className='logo-container'>
-        <img src={LogoImg} alt='Logo' className='logo' />
-        <FiLogOut className='logout-button' onClick={handleLogout} />
+    <div className='dashboard-wrapper'>
+      <div className='dashboard-container'>
+        <div className='logo-container'>
+          <img src={LogoImg} alt='Logo' className='logo' />
+          <FiLogOut className='logout-button' onClick={handleLogout} />
+        </div>
+        <div className='options-container'>
+          <FiSettings className='settings-button' onClick={handleLogout} />
+        </div>
+        <div className='tasks-table-container'>
+          <TasksTable className='task-table' />
+        </div>
       </div>
-      <div className='options-container'>
-        <FiSettings className='settings-button' onClick={handleLogout} />
-      </div>
-      <TasksTable />
     </div>
   );
 }
